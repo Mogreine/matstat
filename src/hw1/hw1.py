8e6d70acc66a04e2577cb4c7e24e3dcb6f96510f
Mogreine <north7311@gmail.com>
2020-11-07 18:47:32 +0300

    Refactoring

--- src/hw1/hw1.py
+++ src/hw1/hw1.py
@@ -11,7 +11,6 @@ def plot(x, uni, exp):
         marker={'color': 'rgba(0, 255, 0, 0.8)'},
         name='uni',
         xaxis='x1',
-        yaxis='y1'
     )
     trace2 = go.Scatter(
         x=x,
@@ -25,19 +24,31 @@ def plot(x, uni, exp):
     data = [trace1, trace2]
 
     layout = go.Layout(
-        title='Зависимость std от k',
-        xaxis=dict(
-            domain=[0, 0.45]
-        ),
-        yaxis=dict(
-            domain=[0, 0.65]
+        title={
+            # 'text': "$\theta_{uni} = 20,\; \theta_{exp} = 10$",
+            'y': 0.9,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'},
+        xaxis1=dict(
+            domain=[0, 0.45],
+            title='k'
         ),
         xaxis2=dict(
-            domain=[0.55, 1]
+            domain=[0.55, 1],
+            title='k'
+        ),
+        yaxis1=dict(
+            anchor='x1',
+            title='mse',
+            showexponent='all',
+            exponentformat='e'
         ),
         yaxis2=dict(
-            domain=[0, 0.65],
-            anchor='x2'
+            anchor='x2',
+            title='mse',
+            showexponent='all',
+            exponentformat='e'
         )
     )
 
@@ -45,11 +56,11 @@ def plot(x, uni, exp):
     plotly.offline.iplot(fig)
 
 
-def gen_uni(theta, n=10000):
+def gen_uni(theta, n=1000):
     return np.random.uniform(0, theta, n)
 
 
-def gen_exp(theta, n=10000):
+def gen_exp(theta, n=1000):
     return np.random.exponential(theta, n)
 
 
@@ -61,32 +72,34 @@ def eval_param_exp(data_k, k):
     return (np.mean(data_k) / np.math.factorial(k)) ** (1 / k)
 
 
-def eval_std(data_gen_func, eval_func, n, s, k, theta):
+def eval_mse(data_gen_func, eval_func, n, s, k, theta):
     theta_eval_arr = []
     for i in range(1, s + 1):
         data = data_gen_func(theta, n)
         data_k = data ** k
         theta_eval = eval_func(data_k, k)
         theta_eval_arr.append(theta_eval)
-    res = std(np.array(theta_eval_arr), theta)
+    res = mse(np.array(theta_eval_arr), theta)
     return res
 
 
-def std(arr, param):
-    return np.sum((arr - param) ** 2) / len(arr)
+def mse(arr, param):
+    e = arr - param
+    return e @ e / len(e)
+
+
+def calc(uni_theta, exp_theta, n=1000, s=100):
+    exp_mse_arr, uni_mse_arr = [], []
+    k_arr = [*range(1, 101)]
+    for k in k_arr:
+        uni_mse_k = eval_mse(gen_uni, eval_param_uni, n, s, k, uni_theta)
+        exp_mse_k = eval_mse(gen_exp, eval_param_exp, n, s, k, exp_theta)
+
+        uni_mse_arr.append(uni_mse_k)
+        exp_mse_arr.append(exp_mse_k)
+
+    plot(k_arr, uni_mse_arr, exp_mse_arr)
 
 
 if __name__ == "__main__":
-    uni_theta, exp_theta = 100, 10
-    n, s = 1000, 100
-
-    exp_std_arr, uni_std_arr = [], []
-    k_arr = [*range(1, 101)]
-    for k in k_arr:
-        uni_std_k = eval_std(gen_uni, eval_param_uni, n, s, k, uni_theta)
-        exp_std_k = eval_std(gen_exp, eval_param_exp, n, s, k, exp_theta)
-
-        uni_std_arr.append(uni_std_k)
-        exp_std_arr.append(exp_std_k)
-
-    plot(k_arr, uni_std_arr, exp_std_arr)
+    calc(20, 20)
